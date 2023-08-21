884a02aef1acdf20b2455686ae83c37228cd768c
RehanMaroof <rehanmaroof000@gmail.com>
2023-08-21 11:04:27 +0500

    Update: CRUD operation

--- src/resolvers/updateBrandPayload.js
+++ src/resolvers/updateBrandPayload.js
@@ -1,29 +1,25 @@
-import ObjectID from "mongodb";
+
+
 export default {
   async brandProduct(parent, args, context, info) {
     try {
       const { collections } = context;
       const { Products } = collections;
       const { _id } = parent;
-      console.log("parent", typeof parent._id);
-      console.log("parent", parent);
-      console.log(
-        "ObjectID.ObjectId(parent._id)",
-        ObjectID.ObjectId(parent._id)
-      );
-      let proderesp = await Products.find({
-        brandId: ObjectID.ObjectId(parent._id),
-      }).toArray();
-      // const products = await Products.aggregate([
-      //   {
-      //     $match: { brandId: parent._id }
-      //   }
-      // ]).toArray();
-      console.log("products", proderesp);
-      return proderesp;
+    
+      // Step 1: Fetch all products
+      const allProducts = await Products.find(
+        { brandId: { $exists: true } },
+      ).toArray();
+      console.log("allProducts", allProducts);
+      
+      // Step 2: Filter products by brandId using Array.filter
+      const brandProducts = allProducts.filter(product => product.brandId.toString() === _id.toString());
+      
+      console.log("brandProducts", brandProducts);
+      return brandProducts;
     } catch (error) {
-      console.error("Error fetching brand products:", error);
-      // throw error;
+      throw new Error(error.message);
     }
   },
 };
