a299c2cb8bb271c919e98e6375c81a50807f86bc
RAMZAN494 <bhutta69316@gmail.com>
2024-11-24 17:34:18 +0500

    ad:EmailUpdate

--- src/mutations/updateSellerInfo.js
+++ src/mutations/updateSellerInfo.js
@@ -58,6 +58,95 @@
 //   return updatedAccountResp.value;
 // }
 
+// import ReactionError from "@reactioncommerce/reaction-error";
+
+// export default async function updateSellerInfo(context, input) {
+//   if (!context.user) {
+//     throw new ReactionError("access-denied", "Access Denied");
+//   }
+
+//   const { Accounts } = context.collections;
+//   const {
+//     _id,
+//     image,
+//     storeName,
+//     pickUpAddress,
+//     city,
+//     contactNumber,
+//     name,
+//     bankDetail,
+//     documentDetails,
+//     profilePicture,
+//     email
+//   } = input;
+
+//   const updates = {};
+//   const updatedFields = [];
+
+//   // Handle individual fields update
+//   if (image) updates["storeLogo"] = image;
+//   if (storeName) updates["storeName"] = storeName;
+//   if (pickUpAddress) updates["pickUpAddress"] = pickUpAddress;
+//   if (city) updates["storeAddress.city"] = city;
+//   if (contactNumber) updates["contactNumber"] = contactNumber;
+//   if (name) updates["name"] = name;
+//   if (email) updates["emails.0.address"] = email;
+//   if (profilePicture) {
+//     updates["profile.picture"] = profilePicture;
+//     updatedFields.push("profile.picture");
+//   }
+
+
+
+//   if (bankDetail) {
+//     const activeBankDetailsCount = bankDetail.filter((detail) => detail.isActive).length;
+
+//     if (activeBankDetailsCount > 1) {
+//       throw new ReactionError(
+//         "multiple-active-bank-details",
+//         "Only one bank detail can be active at a time. Please ensure only one bank detail is set to active."
+//       );
+//     }
+
+//     updates["bankDetail"] = bankDetail;
+//     updatedFields.push("bankDetail");
+//   }
+
+//   if (documentDetails) {
+//     updates["documentDetails"] = documentDetails;
+//     updatedFields.push("documentDetails");
+//   }
+
+//   const updatedAccountResp = await Accounts.findOneAndUpdate(
+//     { _id },
+//     { $set: updates },
+//     { returnOriginal: false }
+//   );
+
+//   const updatedAccount = updatedAccountResp.value;
+
+//   if (!updatedAccount) {
+//     throw new ReactionError("not-found", "Account not found");
+//   }
+
+//   // Construct the response object explicitly
+//   return {
+//     _id: updatedAccount._id || null,
+//     image: updatedAccount.storeLogo || null,
+//     storeName: updatedAccount.storeName || null,
+//     pickUpAddress: updatedAccount.pickUpAddress || null,
+//     city: updatedAccount.storeAddress?.city || null,
+//     contactNumber: updatedAccount.contactNumber || null,
+//     name: updatedAccount.name || null,
+//     profilePicture: updatedAccount.profile?.picture || null,
+//     email: updatedAccount.emails?.[0].address || null,
+//     bankDetail: updatedAccount.bankDetail || null,
+//   };
+
+// }
+
+
+
 import ReactionError from "@reactioncommerce/reaction-error";
 
 export default async function updateSellerInfo(context, input) {
@@ -65,7 +154,7 @@ export default async function updateSellerInfo(context, input) {
     throw new ReactionError("access-denied", "Access Denied");
   }
 
-  const { Accounts } = context.collections;
+  const { Accounts, users } = context.collections; // Add Users collection
   const {
     _id,
     image,
@@ -76,7 +165,8 @@ export default async function updateSellerInfo(context, input) {
     name,
     bankDetail,
     documentDetails,
-    profilePicture
+    profilePicture,
+    email
   } = input;
 
   const updates = {};
@@ -89,6 +179,7 @@ export default async function updateSellerInfo(context, input) {
   if (city) updates["storeAddress.city"] = city;
   if (contactNumber) updates["contactNumber"] = contactNumber;
   if (name) updates["name"] = name;
+  if (email) updates["emails.0.address"] = email;
   if (profilePicture) {
     updates["profile.picture"] = profilePicture;
     updatedFields.push("profile.picture");
@@ -113,6 +204,7 @@ export default async function updateSellerInfo(context, input) {
     updatedFields.push("documentDetails");
   }
 
+  // Update the Accounts collection
   const updatedAccountResp = await Accounts.findOneAndUpdate(
     { _id },
     { $set: updates },
@@ -125,6 +217,21 @@ export default async function updateSellerInfo(context, input) {
     throw new ReactionError("not-found", "Account not found");
   }
 
+  // If email is updated, update it in the Users collection as well
+  if (email) {
+    const userUpdateResp = await users.findOneAndUpdate(
+      { _id },
+      { $set: { "emails.0.address": email } },
+      { returnOriginal: false }
+    );
+
+    const updatedUser = userUpdateResp.value;
+
+    if (!updatedUser) {
+      throw new ReactionError("not-found", "User not found for email update");
+    }
+  }
+
   // Construct the response object explicitly
   return {
     _id: updatedAccount._id || null,
@@ -135,7 +242,7 @@ export default async function updateSellerInfo(context, input) {
     contactNumber: updatedAccount.contactNumber || null,
     name: updatedAccount.name || null,
     profilePicture: updatedAccount.profile?.picture || null,
+    email: updatedAccount.emails?.[0].address || null,
     bankDetail: updatedAccount.bankDetail || null,
   };
-
 }
